494b8230820a4ea45e10f7879cf1d369d96954b4
Lakshya sharma <64037374+LAKSHYA9941@users.noreply.github.com>
2025-03-05 14:43:59 +0000

    datas of all types

--- JSB/datatypes.js
+++ JSB/datatypes.js
@@ -1,4 +1,3 @@
-"use strict";
 console.log("lakshya added;-" + 5 + 9);
 const k=90**89
 console.log( k)
---
+++ JSB/types-of-data.js
@@ -0,0 +1,29 @@
+d1="good"
+d2=3490000000000000008111111n
+d3=false
+d4=Symbol(89)
+d5 = function(){
+    a=90
+    b=89
+    console.log("a+b= "+(a+b));
+        
+}
+d6=null
+let d7;
+d8=2
+d9=['i','n','b','c','w','r','s','f','g','h']
+d10={
+    names:"o",
+    cart:9029100002
+}
+
+console.log(typeof d1);
+console.log(typeof d2);
+console.log(typeof d3);
+console.log(typeof d4);
+console.log(typeof d5);
+console.log(typeof d6);
+console.log(typeof d7);
+console.log(typeof d8);
+console.log(typeof d9);
+console.log(typeof d10);
